720893c72fe485183bd696164e74a07971774226
Parul Verma <156315453+Haroldojo@users.noreply.github.com>
2025-11-07 17:35:05 +0530

    Update settings.py

--- promptify/settings.py
+++ promptify/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    os.getenv("RENDER_EXTERNAL_HOSTNAME"),
-]
+ALLOWED_HOSTS = [https://chatfrontend-hnfe.onrender.com,]
 
 
 # Application definition
